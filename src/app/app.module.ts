e2e8888b8753968de8c87c6cf96015018f1afa25
yohann <yohannmartinez34@gmail.com>
2018-04-05 16:50:02 +0200

    routes add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { SelectcharacterComponent } from './selectcharacter/selectcharacter.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'game', component: GameComponent}, 
+  { path: 'selectcharacter', component: SelectcharacterComponent},
+   
+]
 
 
 @NgModule({
@@ -16,7 +24,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
